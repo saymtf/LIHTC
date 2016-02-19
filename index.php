10f9940cc77d1317d3113048f43c919039a7152d
Anonymous <anonymous@noemail.com>
2016-02-19 20:46:33 +0000

    added jquery

--- index.php
+++ index.php
@@ -4,14 +4,20 @@ GIT REPO:
 
 https://github.com/saymtf/LIHTC.git 
 
--->
+-->  
 <!DOCTYPE html>
 <html>
+  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  
 <head>
     <title>CST-499</title>
 </head>
+  
 <body>
 	<h1>Hello Codenvy</h1>
-  <input type="text">
+	<input type="text">
 </body>
+   <script>
+     
+   </script>
 </html>
