d1589d001593ec03796acc3a5dd917029d79e472
Anonymous <anonymous@noemail.com>
2016-02-24 20:46:16 +0000

    Added GMaps

--- index.php
+++ index.php
@@ -16,6 +16,24 @@ https://github.com/saymtf/LIHTC.git
     <title>CST-499</title>
 </head>
   
+<script>
+    function initMap() {
+    		<!-- Change Location -->
+  			var myLatLng = {lat: -25.363, lng: 131.044};
+	
+  			var map = new google.maps.Map(document.getElementById('map'), {
+    				zoom: 4,
+    				center: myLatLng
+  			});
+
+  			var marker = new google.maps.Marker({
+    			position: myLatLng,
+    			map: map,
+    			title: 'Hello World!'
+  			});
+		}
+</script>
+  
 <body>
 	<div id="wrapper">
      
