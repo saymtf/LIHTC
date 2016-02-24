e678bdd989299662abd5bb63ab67d8b3c3d5fdea
Anonymous <anonymous@noemail.com>
2016-02-24 20:38:09 +0000

    Added Divs and some images that we plan to use

--- index.php
+++ index.php
@@ -1,28 +1,48 @@
-/*
-
+<!-- 
 GIT REPO:
 
 https://github.com/saymtf/LIHTC.git 
-#1589FF // BLUE
-#5EFB6E // GREEN
-*/
-
+#1589FF BLUE
+#5EFB6E GREEN
+-->
+  
 <!DOCTYPE html>
 <html>
-  <?php
-	echo "Hello";
-	?>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
 <head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
     <title>CST-499</title>
 </head>
   
 <body>
-	<h1>Hello Codenvy</h1>
-	<input type="text">
-</body>
-   <script>
+	<div id="wrapper">
      
-   </script>
+     <!-- Display First -->
+     <div id="images">
+       
+     		<div id="home">
+      		<i class="fa fa-home fa-5x"></i>
+     		</div>  
+      
+      	<div id="building">
+      		<i class="fa fa-building-o fa-5x"></i>
+     		</div>  
+            
+      </div>
+            
+      <!-- Text Fields -->
+      <div id="text_fields">
+		</div>        
+      
+      <!-- Display Map -->
+      <div id="google_maps">
+      </div>
+            
+  </div>
+</body>
+  <script>
+  		$('#images').blur()
+  </script>
 </html>
